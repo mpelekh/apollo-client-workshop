658bf86bf120670898e9be6e221cdcc2987f92ee
Mykola Pelekh <mpelekh@lohika.com>
2019-05-30 19:49:48 +0300

    Install needed dependencies and try to fetch data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
+import ApolloClient, { gql } from "apollo-boost"
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -11,6 +12,23 @@ const signedInUser = { email: 'mpelekh@lohika.com' }
 export const UserContext = React.createContext({
   email: 'guest@lohika.com'
 })
+
+const client = new ApolloClient({
+	uri: "http://localhost:4000/graphql"
+});
+
+client
+	.query({
+		query: gql`
+			{
+				post(postId: 1) {
+					id
+					title
+				}
+			}
+		`
+	})
+	.then(result => console.log(result));
 
 render(
   <Provider store={store}>
