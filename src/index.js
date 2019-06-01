b29aec6c58979d2f5b2d81cc53370618287a504b
Mykola Pelekh <mpelekh@lohika.com>
2019-06-01 12:29:55 +0300

    Add websocket link

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import * as serviceWorker from './serviceWorker'
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
 import { onError } from 'apollo-link-error'
-import { ApolloLink } from 'apollo-link'
+import { ApolloLink, split } from 'apollo-link'
 import { ApolloProvider } from 'react-apollo'
+import { getMainDefinition } from 'apollo-utilities'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -29,10 +31,27 @@ const client = new ApolloClient({
         )
       if (networkError) console.log(`[Network error]: ${networkError}`)
     }),
-    new HttpLink({
-      uri: 'http://localhost:4000/graphql',
-      credentials: 'same-origin'
-    })
+
+    split(
+      // split based on operation type
+      ({ query }) => {
+        const definition = getMainDefinition(query)
+        return (
+          definition.kind === 'OperationDefinition' &&
+          definition.operation === 'subscription'
+        )
+      },
+      new WebSocketLink({
+        uri: 'ws://localhost:4000/graphql',
+        options: {
+          reconnect: true
+        }
+      }),
+      new HttpLink({
+        uri: 'http://localhost:4000/graphql',
+        credentials: 'same-origin'
+      })
+    )
   ]),
   cache: new InMemoryCache()
 })
